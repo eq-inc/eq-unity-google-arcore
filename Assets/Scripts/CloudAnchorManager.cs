7472c780eef563b533f7367c83ce743518281058
EQ_htatsuguchi <tatsuguchi@eq-inc.jp>
2018-06-14 14:03:02 +0900

    一時保存

---
+++ Assets/Scripts/CloudAnchorManager.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class CloudAnchorManager
+{
+    private static readonly string PrefKeyIntAnchorCount = "PrefKeyIntAnchorCount";
+    private static readonly string PrefKeyStringAnchor = "PrefKeyStringAnchor_{0}";
+
+    private static Dictionary<string, string> sPrefKeyDic = new Dictionary<string, string>();
+    private static Dictionary<string, string> sAnchorIdKeyDic = new Dictionary<string, string>();
+
+    public static int Count
+    {
+        get
+        {
+            RestoreAll();
+            return sPrefKeyDic.Count;
+        }
+    }
+
+    public static bool GetCloudAnchorId(int index, out string value)
+    {
+        bool ret = true;
+        string key = string.Format(PrefKeyStringAnchor, index);
+
+        lock (sPrefKeyDic)
+        {
+            if (!sPrefKeyDic.TryGetValue(key, out value))
+            {
+                value = PlayerPrefs.GetString(key, null);
+                if (value == null)
+                {
+                    ret = false;
+                }
+                else
+                {
+                    sPrefKeyDic[key] = value;
+                    sAnchorIdKeyDic[value] = key;
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    public static void Append(string value)
+    {
+        lock (sPrefKeyDic)
+        {
+            if (!sPrefKeyDic.ContainsValue(value))
+            {
+                int nextIndex = sPrefKeyDic.Count;
+                string key = string.Format(PrefKeyStringAnchor, nextIndex);
+
+                sPrefKeyDic[key] = value;
+                sAnchorIdKeyDic[value] = key;
+                PlayerPrefs.SetString(key, value);
+                PlayerPrefs.Save();
+            }
+        }
+    }
+
+    public static void Remove(string value)
+    {
+        lock (sPrefKeyDic)
+        {
+            string key = null;
+            if (sAnchorIdKeyDic.TryGetValue(value, out key))
+            {
+                sAnchorIdKeyDic.Remove(value);
+                sPrefKeyDic.Remove(key);
+
+                PlayerPrefs.DeleteKey(key);
+            }
+        }
+    }
+
+    private static void RestoreAll()
+    {
+        int count = PlayerPrefs.GetInt(PrefKeyIntAnchorCount);
+
+        lock (sPrefKeyDic)
+        {
+            sPrefKeyDic.Clear();
+            for (int i = 0; i < count; i++)
+            {
+                string key = string.Format(PrefKeyStringAnchor, i);
+                string value = sPrefKeyDic[key] = PlayerPrefs.GetString(key);
+                sAnchorIdKeyDic[value] = key;
+            }
+        }
+    }
+
+    public static void SaveAll()
+    {
+        lock (sPrefKeyDic)
+        {
+            PlayerPrefs.SetInt(PrefKeyIntAnchorCount, sPrefKeyDic.Count);
+
+            foreach (KeyValuePair<string, string> kvPair in sPrefKeyDic)
+            {
+                PlayerPrefs.SetString(kvPair.Key, kvPair.Value);
+            }
+
+            PlayerPrefs.Save();
+        }
+    }
+}
---
+++ Assets/Scripts/TestCloudAnchor.cs
@@ -0,0 +1,78 @@
+﻿using Eq.Unity;
+using GoogleARCore;
+using GoogleARCore.CrossPlatform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class TestCloudAnchor : BaseAndroidMainController
+{
+    public GameObject mAnchorPrefab = null;
+    private Dictionary<string, XPAnchor> mCloudAnchorDic = new Dictionary<string, XPAnchor>();
+
+    internal override void Start()
+    {
+        base.Start();
+    }
+
+    internal override void Update()
+    {
+        base.Update();
+
+        if(Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+
+        Touch lastTouched;
+        if(LastTouch(out lastTouched))
+        {
+            if(lastTouched.phase == TouchPhase.Ended)
+            {
+                TrackableHit hit;
+                if (Frame.Raycast(lastTouched.position.x, lastTouched.position.y, TrackableHitFlags.PlaneWithinPolygon | TrackableHitFlags.FeaturePointWithSurfaceNormal, out hit))
+                {
+                    // Anchorを生成
+                    Anchor anchor = Session.CreateAnchor(hit.Pose, hit.Trackable);
+
+                    // CloudAnchorを生成
+                    XPSession.CreateCloudAnchor(anchor).ThenAction(delegate(CloudAnchorResult result)
+                    {
+                        XPAnchor xpAnchor = result.Anchor;
+
+                        if(xpAnchor != null)
+                        {
+
+                            mCloudAnchorDic[xpAnchor.CloudId] = xpAnchor;
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+    public void RestoreAnchorsButtonClicked()
+    {
+        int anchorCount = CloudAnchorManager.Count;
+        string anchorId = null;
+
+        mCloudAnchorDic.Clear();
+        for (int i=0; i<anchorCount; i++)
+        {
+            if(CloudAnchorManager.GetCloudAnchorId(i, out anchorId))
+            {
+                XPSession.ResolveCloudAnchor(anchorId).ThenAction(
+                    delegate (CloudAnchorResult result)
+                    {
+                        XPAnchor anchor = result.Anchor;
+                        mCloudAnchorDic[anchor.CloudId] = anchor;
+                    }
+                );
+            }
+        }
+    }
+}
+
+
