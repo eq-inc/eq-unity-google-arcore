ab613e8abe52451e1f7cc7b68fda9543a7ae9d1c
EQ_htatsuguchi <tatsuguchi@eq-inc.jp>
2018-06-18 12:54:47 +0900

    一時保存

--- Assets/Scripts/TestCloudAnchor.cs
+++ Assets/Scripts/TestCloudAnchor.cs
@@ -7,15 +7,23 @@ using UnityEngine.UI;
 
 public class TestCloudAnchor : BaseAndroidMainController
 {
+    private static readonly float AnimationInterval = 5f;
+    private static readonly float MinimumAnimationInterval = 1f / 5;
+
     public GameObject mButtonGroupCanvasGO;
     public GameObject mListCloudAnchorPanelGO;
     public GameObject mAddCloudAnchorPanelGO;
     public GameObject mAnchorPrefab = null;
     public GameObject mListItemPrefab = null;
 
+    private Dictionary<string, Anchor> mAnchorDic = new Dictionary<string, Anchor>();
     private Dictionary<string, XPAnchor> mCloudAnchorDic = new Dictionary<string, XPAnchor>();
     private Dictionary<string, GameObject> mCloudAnchorGODic = new Dictionary<string, GameObject>();
-    private UnityEngine.UI.InputField mIfCloudAnchorId;
+    private InputField mIfCloudAnchorId;
+    private float mLeftAnimationInterval = 0f;
+    private float mLeftOneAnimationInterval = 0f;
+    private bool mUpdateAnchorsPosition = false;
+    System.Random mRandom = new System.Random();
 
     internal override void Start()
     {
@@ -26,12 +34,12 @@ public class TestCloudAnchor : BaseAndroidMainController
     {
         base.Update();
 
-        if(Session.Status != SessionStatus.Tracking)
+        if (Session.Status != SessionStatus.Tracking)
         {
             return;
         }
 
-        if((mListCloudAnchorPanelGO.activeInHierarchy == false) && (mAddCloudAnchorPanelGO.activeInHierarchy == false))
+        if ((mListCloudAnchorPanelGO.activeInHierarchy == false) && (mAddCloudAnchorPanelGO.activeInHierarchy == false))
         {
             Touch lastTouched;
             if (LastTouch(out lastTouched))
@@ -46,8 +54,7 @@ public class TestCloudAnchor : BaseAndroidMainController
 
                         GameObject anchorGO = Instantiate(mAnchorPrefab, anchor.transform.position, anchor.transform.rotation);
                         anchorGO.SetActive(true);
-
-                        mLogger.CategoryLog(LogCategoryMethodTrace, "create local anchor");
+                        anchorGO.transform.parent = anchor.transform;
 
                         // CloudAnchorを生成
                         XPSession.CreateCloudAnchor(anchor).ThenAction(delegate (CloudAnchorResult result)
@@ -56,11 +63,60 @@ public class TestCloudAnchor : BaseAndroidMainController
 
                             if (xpAnchor != null)
                             {
-                                mLogger.CategoryLog(LogCategoryMethodTrace, "create cloud anchor: id = " + xpAnchor.CloudId);
-                                AddAnchorGO(xpAnchor, anchorGO);
+                                AddAnchorGO(xpAnchor, anchorGO, anchor);
                             }
                         });
                     }
+                }
+            }
+        }
+
+        // アニメーション
+        bool enableAnimation = false;
+        if (mLeftAnimationInterval > 0)
+        {
+            mLeftAnimationInterval -= Time.deltaTime;
+            mLeftOneAnimationInterval -= Time.deltaTime;
+
+            if(mLeftOneAnimationInterval <= 0)
+            {
+                enableAnimation = true;
+                mLeftOneAnimationInterval = MinimumAnimationInterval;
+            }
+        }
+
+        // 表示位置の更新
+        if(mCloudAnchorDic.Count > 0)
+        {
+            foreach (string cloudAnchorId in mAnchorDic.Keys)
+            {
+                Anchor anchor = mAnchorDic[cloudAnchorId];
+                GameObject cloudAnchorGO = null;
+
+                if (enableAnimation)
+                {
+                    int direction = (mRandom.Next() * mRandom.Next()) % 3;
+                    double distanceSource = mRandom.NextDouble() * mRandom.NextDouble();
+                    float distance = ((float)(distanceSource - System.Math.Floor(distanceSource)) / 1);
+                    if (direction == 0)
+                    {
+                        anchor.transform.position = new Vector3(anchor.transform.position.x + distance, anchor.transform.position.y, anchor.transform.position.z);
+                    }
+                    else if (direction == 1)
+                    {
+                        anchor.transform.position = new Vector3(anchor.transform.position.x, anchor.transform.position.y + distance, anchor.transform.position.z);
+                    }
+                    else
+                    {
+                        anchor.transform.position = new Vector3(anchor.transform.position.x, anchor.transform.position.y, anchor.transform.position.z + distance);
+                    }
+                }
+
+                if ((enableAnimation || mUpdateAnchorsPosition) && mCloudAnchorGODic.TryGetValue(cloudAnchorId, out cloudAnchorGO))
+                {
+                    cloudAnchorGO.transform.SetPositionAndRotation(anchor.transform.position, anchor.transform.rotation);
+
+                    mUpdateAnchorsPosition = false;
                 }
             }
         }
@@ -73,15 +129,15 @@ public class TestCloudAnchor : BaseAndroidMainController
         string anchorId = null;
 
         mCloudAnchorDic.Clear();
-        for (int i=0; i<anchorCount; i++)
+        for (int i = 0; i < anchorCount; i++)
         {
-            if(CloudAnchorManager.GetCloudAnchorId(i, out anchorId))
+            if (CloudAnchorManager.GetCloudAnchorId(i, out anchorId))
             {
                 XPSession.ResolveCloudAnchor(anchorId).ThenAction(
                     delegate (CloudAnchorResult result)
                     {
                         XPAnchor xpAnchor = result.Anchor;
-                        if(xpAnchor != null)
+                        if (xpAnchor != null)
                         {
                             AddAnchorGO(xpAnchor);
                         }
@@ -101,7 +157,7 @@ public class TestCloudAnchor : BaseAndroidMainController
 
         // 一旦子要素を全て解放
         string cloudAnchorId = null;
-        for(int i=0, size= cloudAnchorListContentGO.transform.childCount; i<size; i++)
+        for (int i = 0, size = cloudAnchorListContentGO.transform.childCount; i < size; i++)
         {
             GameObject childGO = cloudAnchorListContentGO.transform.GetChild(i).gameObject;
             Destroy(childGO);
@@ -109,26 +165,26 @@ public class TestCloudAnchor : BaseAndroidMainController
         cloudAnchorListContentGO.transform.DetachChildren();
 
         int cloudAnchorCount = CloudAnchorManager.Count;
-        mLogger.CategoryLog(LogCategoryMethodTrace, "CloudAnchorManager.Count = " + cloudAnchorCount);
 
         // Scroll Viewの設定
         float itemHeight = mListItemPrefab.GetComponent<LayoutElement>().preferredHeight;
         float itemVerticalSpace = cloudAnchorListContentGO.GetComponent<VerticalLayoutGroup>().spacing;
         cloudAnchorListContentGO.GetComponent<RectTransform>().sizeDelta = new Vector2(0, (itemHeight + itemVerticalSpace) * cloudAnchorCount);
 
-        for (int i=0, size=CloudAnchorManager.Count; i<size; i++)
+        for (int i = 0, size = CloudAnchorManager.Count; i < size; i++)
         {
-            if(CloudAnchorManager.GetCloudAnchorId(i, out cloudAnchorId))
+            if (CloudAnchorManager.GetCloudAnchorId(i, out cloudAnchorId))
             {
                 XPSession.ResolveCloudAnchor(cloudAnchorId).ThenAction((CloudAnchorResult result) =>
                 {
                     XPAnchor xpAnchor = result.Anchor;
 
-                    if(xpAnchor != null)
+                    if (xpAnchor != null)
                     {
                         // リストに追加
                         GameObject listItemGO = Instantiate(mListItemPrefab);
                         //listItemGO.transform.Find("CloudAnchorId").gameObject.GetComponent<UnityEngine.UI.Text>().text = xpAnchor.CloudId;
+                        mLogger.CategoryLog(LogCategoryMethodTrace, "cloud anchor id = " + xpAnchor.CloudId);
                         listItemGO.GetComponent<UnityEngine.UI.Text>().text = xpAnchor.CloudId;
                         listItemGO.transform.SetParent(cloudAnchorListContentGO.transform);
                     }
@@ -190,7 +246,7 @@ public class TestCloudAnchor : BaseAndroidMainController
             XPSession.ResolveCloudAnchor(inputAnchorId).ThenAction((CloudAnchorResult result) =>
             {
                 XPAnchor xpAnchor = result.Anchor;
-                if(xpAnchor != null)
+                if (xpAnchor != null)
                 {
                     AddAnchorGO(xpAnchor);
                 }
@@ -206,18 +262,34 @@ public class TestCloudAnchor : BaseAndroidMainController
         mLogger.CategoryLog(LogCategoryMethodOut);
     }
 
-    private void AddAnchorGO(XPAnchor xpAnchor, GameObject anchorGO = null)
+    public void MoveAnchorsButtonClicked()
+    {
+        mLogger.CategoryLog(LogCategoryMethodIn);
+        mLeftAnimationInterval = AnimationInterval;
+        mLeftOneAnimationInterval = 0;
+        mLogger.CategoryLog(LogCategoryMethodOut);
+    }
+
+    public void UpdateAnchorsButtonClicked()
+    {
+        mLogger.CategoryLog(LogCategoryMethodIn);
+        mUpdateAnchorsPosition = true;
+        mLogger.CategoryLog(LogCategoryMethodOut);
+    }
+
+    private void AddAnchorGO(XPAnchor xpAnchor, GameObject anchorGO = null, Anchor anchor = null)
     {
         mLogger.CategoryLog(LogCategoryMethodIn);
 
-        if(!mCloudAnchorGODic.TryGetValue(xpAnchor.CloudId, out anchorGO))
+        if (anchorGO != null || !mCloudAnchorGODic.TryGetValue(xpAnchor.CloudId, out anchorGO))
         {
-            if(anchorGO == null)
+            if (anchorGO == null)
             {
                 anchorGO = Instantiate(mAnchorPrefab, xpAnchor.transform.position, xpAnchor.transform.rotation);
             }
             anchorGO.SetActive(true);
 
+            mAnchorDic[xpAnchor.CloudId] = anchor;
             mCloudAnchorDic[xpAnchor.CloudId] = xpAnchor;
             mCloudAnchorGODic[xpAnchor.CloudId] = anchorGO;
             CloudAnchorManager.Append(xpAnchor.CloudId);
